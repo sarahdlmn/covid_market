d2c4e375da8203f01df67272deee9a5a9571e59d
UniVindY <univindy@gmail.com>
2020-03-19 14:55:10 +0100

    Carte intégrée

--- maps/maps.php
+++ maps/maps.php
@@ -17,7 +17,20 @@
         echo '<div id="mapid" class="col-12">';
     }
  ?>
-Affichage de la map leaft 
+
+<script language="javascript" >
+            var mymap = L.map('mapid').setView([48.6833, 6.2], 13);
+
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiYWxleHljZiIsImEiOiJjazd5anZ4cGIwMnQ0M2ZwOWp5ZmM4eW05In0.5l3noRduV0z5a0XCPbTsfA'
+        }).addTo(mymap);
+            </script>
+ </div>
 <br>
 <form action="#" method="post">
 <input type="submit" name="vrai" value="Avec Marqueur"><br>
