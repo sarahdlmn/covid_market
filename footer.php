808a8140d36f6e5fbf2bef1aa7017f46108b626d
UniVindY <univindy@gmail.com>
2020-03-19 13:58:16 +0100

    13h58

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </body>
 <footer>
-<a name="" id="" class="btn btn-primary" href="./dashboard/connexion/connexion.php" role="button">Magasin, se connecter</a>
+<a name="" id="btn-footer" class="btn btn-primary" href="./dashboard/connexion/connexion.php" role="button">Magasin, se connecter</a>
 </footer>
 
 </html>
--- maps/maps.php
+++ maps/maps.php
@@ -1,5 +1,14 @@
-
-<div id="mapid"><br><br>
+<section class="row">
+    <?php
+    $marqeur=null;
+        if ( $marqeur != null){
+            echo '<div class="col-6">';
+            include 'maps-side.php';
+            echo '</div>';
+            echo '<div id="mapid" class="col-6">';}else{
+            echo '<div id="mapid" class="col-12">';
+        }
+ ?>
 Affichage de la map leaft
-<br><br>
 </div>
+</section>
