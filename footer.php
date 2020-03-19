8bd4fed9744fd64d151bd5f04a6e650bb703e5ec
UniVindY <univindy@gmail.com>
2020-03-19 14:34:56 +0100

    14:34

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </body>
-<footer>
-<a name="" id="btn-footer" class="btn btn-primary" href="./dashboard/connexion/connexion.php" role="button">Magasin, se connecter</a>
+<footer class="d-block p-2 bg-primary text-white">
+<a name="" id="btn-footer" class="btn btn-primary center-block" href="./dashboard/connexion/connexion.php" role="button">Magasin, se connecter</a>
 </footer>
 
 </html>
--- maps/maps.php
+++ maps/maps.php
@@ -1,14 +1,27 @@
 <section class="row">
     <?php
-    $marqeur=null;
-        if ( $marqeur != null){
-            echo '<div class="col-6">';
-            include 'maps-side.php';
-            echo '</div>';
-            echo '<div id="mapid" class="col-6">';}else{
-            echo '<div id="mapid" class="col-12">';
+    $marqueur = null;
+    if(!empty($_POST['test'])){
+       if( $marqueur == 1 ){
+           $marqueur = null;
+        }else{
+        $marqueur=1;
         }
+    }
+
+    if ( $marqueur != null){
+        echo '<div class="col-6">';
+        include 'maps-side.php';
+        echo '</div>';
+        echo '<div id="mapid" class="col-6">';}
+    else{
+        echo '<div id="mapid" class="col-12">';
+    }
  ?>
-Affichage de la map leaft
+Affichage de la map leaft 
+<br>
+<form action="#" method="post">
+<input type="submit" name="test" value="Marqueur">
+</form>
 </div>
 </section>
