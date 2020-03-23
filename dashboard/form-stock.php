9497c4df86da8298ae0407f691795055cb2ad7dd
UniVindY <univindy@gmail.com>
2020-03-23 09:09:27 +0100

    reunion avec louis

--- dashboard/form-stock.php
+++ dashboard/form-stock.php
@@ -1,11 +1,13 @@
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
         crossorigin="anonymous"></script>
         <script src="../assets/js/stock.js"></script>
-
-
 
 <form method="POST" action="#">
   <select id="category">
@@ -25,7 +27,7 @@ $sql = "SELECT * FROM categorie";
      }   
 ?>
   </select>
-</form>
+
 <div>
             <table class="table">
               <thead>
@@ -36,8 +38,13 @@ $sql = "SELECT * FROM categorie";
               </thead>
               <tbody id="produit_ajax">
                  <!-- modification du dom par jquery & ajax                -->
-              <tr><td scope="row">produit test 1 </td><td>15343</td></tr>
+              <tr><td scope="row"><label for="">produit Test</label> </td><td><div class="form-group">
+                <input type="number" min="0"
+                  class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                <small id="helpId" class="form-text text-muted"></small>
+              </div></td></tr>
               </tbody>
             </table>
             <input type="submit" name="" value="Enregistrer" />
 </div>
+</form>
