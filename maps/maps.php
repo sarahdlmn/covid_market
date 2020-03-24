5b823e91b0d49ed777b1410f8fcd54793e8736ee
UniVindY <univindy@gmail.com>
2020-03-24 13:50:11 +0100

    ; de merde

--- maps/maps.php
+++ maps/maps.php
@@ -164,8 +164,7 @@ function onEachFeature(feature, layer) {
                 }).addTo(mymap).on('click', function(e) {
                         console.log(e.latlng);
                         // découpage du coordonee pour passage a en url
-                        window.location.replace('./index.php);
-             ;
+                        window.location.replace('./index.php');
 });               
             </script>
  </div>
