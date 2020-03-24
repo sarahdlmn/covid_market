d759773887d360d9e1d7d690a4cd0b773f866647
WendyZWI <ivy.valentin@hotmail.fr>
2020-03-24 16:42:32 +0100

    mail2

---
+++ dashboard/connexion/mail2.php
@@ -0,0 +1,62 @@
+<?php
+
+    $expediteur='adressedetestmail@gmail.com';
+    $email_to='adressedetestmail@gmail.com';
+    $subject='Essai de message texte';
+    $message='contenu du message (texte) : <strong>TEST</strong>';
+    $headers='From: '.$expediteur. PHP_EOL;
+    $headers.='Content-type:text/plain ;charset=utf-8'.PHP_EOL;
+    
+    if(mail($email_to,$subject,$message,$headers)){
+        echo 'message envoyé';
+    }else{
+        echo 'erreur';
+    }
+
+?>
+
+<?php
+    // pour les spam
+    $SpamErrorMessage="No websites URLs permitted";
+    if (preg_match("/http/i", "$prenom_nom")) {echo "SpamErrorMessage";exit();}
+    if (preg_match("/http/i", "$numero_tel")) {echo "SpamErrorMessage";exit();}
+    if (preg_match("/http/i", "$adresse_mail")) {echo "SpamErrorMessage";exit();}
+    if (preg_match("/http/i", "$message")) {echo "SpamErrorMessage";exit();}
+    if ($anti_spam !=10) {echo "SpamErrorMessage";exit();}
+?>
+
+<div id="page">
+    <div class="content d-flex justify-content-center"> 
+
+        <section>
+            <div>
+                <label for="nom">
+                    <input type="text" name="mail" id="mail" value="" placeholder="Votre mail" />
+                </label>
+            </div>
+            
+            <div class="d-flex justify-content-center"> 
+                <label for="destinataire">
+                    <input type="text" name="to" id="to" value="" placeholder="mail magasin" class="alert alert-success"/>
+                </label>
+            </div>
+
+            <div class="d-flex justify-content-center"> 
+                <label for="sujet">
+                    <input type="text" name="sujet" id="sujet" value="" placeholder="Objet de votre message" />
+                </label>
+            </div>
+
+            <div class="d-flex justify-content-center"> 
+                <label for="message">
+                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                </label>
+            </div>
+
+            <div class="d-flex justify-content-center"> 
+                <input type="submit" name="envoyer" value="Envoyer" id="" />
+            </div>
+        </section>
+
+    </div>
+</div>
