99dc2bdcbea546a89727a9ec807306750107f2b9
UniVindY <univindy@gmail.com>
2020-03-24 08:06:53 +0100

    headerDash

--- dashboard/connexion/connexion.php
+++ dashboard/connexion/connexion.php
@@ -1,4 +1,6 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<?php 
+require '../header-dashboard.php';
+?>
 
 <div class="mt-4">
 <a name="" id="" class="btn btn-primary left ml-4" href="../../index.php" role="button">Retour Carte</a>
